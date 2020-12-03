e31880cb7dff900387f1f3b1af59ee3b926d1e3f
Anderson Bucchianico <34903617+Andybf@users.noreply.github.com>
2020-12-03 01:06:23 -0300

    Add files via upload

---
+++ PDMAula05/PDMAula05/PDMAula05/ViewModel/AlunoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDMAula05.ViewModel {
+    public class AlunoViewModel {
+        #region Propriedades
+        public string RM { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        #endregion
+
+        public AlunoViewModel(Model.Aluno aluno) {
+            this.RM = aluno.RM;
+            this.Nome = aluno.Nome;
+            this.Email = aluno.Email;
+        }
+
+        public static Model.Aluno GetAluno() {
+            var aluno = new Model.Aluno() {
+                Id = Guid.NewGuid(),
+                RM = "54621",
+                Nome = "Anderson Silva",
+                Email = "anderson@ufc.com"
+            };
+            return aluno;
+
+        }
+    }
+}
