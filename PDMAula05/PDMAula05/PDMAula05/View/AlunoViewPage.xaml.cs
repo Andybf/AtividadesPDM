2fb7e6bdf6e4891ad12f0b92d614f6cf239405c3
Anderson Bucchianico <anderson@Sirius.local>
2021-01-08 18:22:24 -0300

    Upload video

---
+++ PDMAula05/PDMAula05/PDMAula05/View/AlunoViewPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDMAula05.View {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlunoViewPage : ContentPage {
+
+        ViewModel.AlunoViewModel vmAluno;
+        public AlunoViewPage() {
+            var aluno = ViewModel.AlunoViewModel.GetAluno();
+            vmAluno = new ViewModel.AlunoViewModel(aluno);
+            BindingContext = vmAluno;
+            InitializeComponent();
+        }
+    }
+}
