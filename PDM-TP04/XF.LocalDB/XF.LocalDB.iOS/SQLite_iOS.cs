34ee08c50e202dbfec7ac89dcaec951dbb9136a7
Anderson Bucchianico <anderson@Sirius.local>
2021-01-08 17:58:54 -0300

    Add TP04

---
+++ PDM-TP04/XF.LocalDB/XF.LocalDB.iOS/SQLite_iOS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using XF.LocalDB.iOS;
+
+using Xamarin.Forms;
+using XF.LocalDB.Data;
+using SQLitePCL;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+namespace XF.LocalDB.iOS {
+    public class SQLite_iOS : sqlite3 {
+        public SQLite_iOS() {
+        }
+        public SQLite.SQLiteConnection GetConexao() {
+            var arquivodb = "ifspdb.db3";
+            string caminho = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string bibliotecaPessoal = Path.Combine(caminho, "..", "Library");
+            var local = Path.Combine(bibliotecaPessoal, arquivodb);
+            var conexao = new SQLite.SQLiteConnection(local);
+            return conexao;
+        }
+    }
+}
---
+++ PDM-TP04/XF.LocalDB/XF.LocalDB/ViewModel/AlunoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF.LocalDB.Model;
+
+namespace XF.LocalDB.ViewModel
+{
+    public class AlunoViewModel {
+        public AlunoViewModel() { }
+        #region Propriedades
+        public string RM { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public List<Aluno> Alunos {
+            get {
+                return App.AlunoModel.GetAlunos().ToList();
+            }
+        }
+        #endregion
+    }
+}
